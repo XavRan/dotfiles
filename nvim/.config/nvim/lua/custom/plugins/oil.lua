96131516277ee83ea89b1fc10dafb6607ef1a007
xavran <xavran@hostv6.entelchile.net>
2024-10-16 15:08:00 -0300

    Updated Oil.nvim settings

--- nvim/.config/nvim/lua/custom/plugins/oil.lua
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -1,12 +1,23 @@
 return {
 	"stevearc/oil.nvim",
+	lazy = false,
 	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
+		columns = {
+			"icon",
+			"permissions",
+			"size",
+			"mtime",
+		},
+		buf_options = {
+			buflisted = true,
+		},
 		view_options = {
 			show_hidden = true,
 		},
 	},
+	constrain_cursor = "name",
 	keys = {
 		{ "<leader>o", ":Oil<CR>", desc = "[O]il File Manager" },
 	},
